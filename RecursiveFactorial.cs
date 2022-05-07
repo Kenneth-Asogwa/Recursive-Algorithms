b8b8c6fde51f9a92d9edeebcce8556dea01288aa
Kenneth Asogwa <kennethasogwa69@gmail.com>
2022-05-07 10:30:00 +0100

    Add files via upload

---
+++ RecursiveFactorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    class RecursiveFactorial
+    {
+        static long Factorial(int n)
+        {
+            if(n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            long factorial = Factorial(n); // function call
+            Console.WriteLine("{0}! = {1}", n, factorial);
+           
+        }
+    }
+}
---
+++ RecursiveFibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonnacci
+{
+    class RecursiveFibonacci
+
+    {
+        static long Fib(int n)
+        {
+            if(n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            long fibonacci = Fib(n);
+            Console.WriteLine("The Fibonacci series at " + n + " is " + fibonacci);
+           
+        }
+    }
+}
